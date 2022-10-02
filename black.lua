fde2999e7a620b8a9c3afd34da825d301839d2ea
medo1423 <114117358+medo1423@users.noreply.github.com>
2022-10-02 21:21:25 +0200

    Add files via upload

--- black.lua
+++ black.lua
@@ -15360,8 +15360,8 @@ keyboard.inline_keyboard = {
 },
 {
 {text = '˹  اضف البوت الي مجموعتك🖲.', url = 't.me/'..UserBot..'?startgroup=new"} 
-}
-}
+}, 
+{
 local msgg = msg_id/2097152/0.5
 https.request("https://api.telegram.org/bot"..Token.."/sendphoto?chat_id=" .. msg_chat_id .. "&photo="..photo.."&caption=".. URL.escape(T).."&reply_to_message_id="..msgg.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 elseif text == 'الاوامر' then
